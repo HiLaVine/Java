2f1b4620f762653a46c389d391abc04728e137b5
HiLaVine <sinnafa01@gmail.com>
2024-02-28 21:38:46 -0600

    java

---
+++ CreacionClasesObjetos/src/main/java/com/mycompany/creacionclasesobjetos/PruebaPersona.java
@@ -0,0 +1,14 @@
+package com.mycompany.creacionclasesobjetos;
+
+public class PruebaPersona {
+
+    public static void main(String[] args) {
+
+        Persona p1 = new Persona();
+        
+        p1.nombre = "Juan";
+        p1.apellido = "Mendez";
+        p1.desplegarNombre();
+    }
+
+}
