b48207affb09c24edfacc65791f34ac837d0a6c8
HiLaVine <sinnafa01@gmail.com>
2024-02-28 21:49:17 -0600

    java

---
+++ PruebaPersona/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Persona p1 = new Persona();
+
+        p1.nombre = "juan";
+        p1.apellido= "garcia";
+
+        p1.desplegarnombres();
+    }
+}
--- PruebaPersona/src/Persona.java
+++ PruebaPersona/src/Persona.java
@@ -1,11 +1,9 @@
-package com.mycompany.creacionclasesobjetos;
-
 public class Persona {
-    
+
     String nombre;
     String apellido;
-    
-    public void desplegarNombre(){
+
+    public void desplegarnombres(){
         System.out.println("Nombre: " + nombre);
         System.out.println("Apellido: " + apellido);
     }
